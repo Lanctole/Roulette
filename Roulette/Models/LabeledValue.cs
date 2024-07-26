7522daa391a13124b4b9f578cc547949416151b4
Alexander Vostrikov <lanctole@yandex.ru>
2024-07-26 13:05:34 +0300

    Созданы необходимые компоненты форм

---
+++ Roulette/Models/LabeledValue.cs
@@ -0,0 +1,13 @@
+﻿namespace Roulette.Models
+{
+    public class LabeledValue
+    {
+        public LabeledValue(string value, string label)
+        {
+            Value = value;
+            Label = label;
+        }
+        public string Value { get; set; }
+        public string Label { get; set; }
+    }
+}
