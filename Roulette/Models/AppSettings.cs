bfc0b6b158505bb7a422aa047c835c1abdd3dd2e
Alexander Vostrikov <lanctole@yandex.ru>
2024-08-01 16:49:20 +0300

    создан сервис для определения домена shikimori

---
+++ Roulette/Models/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Roulette.Models
+{
+    public class AppSettings
+    {
+        public ShikimoriSettings Shikimori { get; set; }
+    }
+
+    public class ShikimoriSettings
+    {
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ Roulette/Services/SettingsService.cs
@@ -0,0 +1,19 @@
+﻿using Roulette.Models;
+
+namespace Roulette.Services
+{
+    public class SettingsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SettingsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ShikimoriSettings GetShikimoriSettings()
+        {
+            return _configuration.GetSection("Shikimori").Get<ShikimoriSettings>();
+        }
+    }
+}
