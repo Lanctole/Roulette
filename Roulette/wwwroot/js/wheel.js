18fa731d3f3d1cc8b9f2202399c307fc2b528187
Alexander Vostrikov <lanctole@yandex.ru>
2024-08-15 11:37:25 +0300

    Вызов SpinFunc вынесен в ShikimoriComponent. Добавлена логика возврата id выигравшего элемента

--- Roulette/wwwroot/js/wheel.js
+++ Roulette/wwwroot/js/wheel.js
@@ -1,89 +1,96 @@
-﻿function createWheel(mediaName) {
-    elements = mediaName;
-    names = [];
-    elements.forEach(element => {
-        names.push(element);
-    });
-    formattedNames = splitAndFormatStrings(names);
-    canvas = document.getElementById('rouletteCanvas');
-    clickedWhat = document.getElementById('clickedWhat');
+﻿formattedNamesToIdMap = [];
+
+function createWheel(mediaName) {
+    const mediaArray = Object.entries(mediaName).map(([Id, name]) => ({ Id: Number(Id), name }));
+    const names = mediaArray.map(item => item.name);
+    const formattedNamesArray = splitAndFormatStrings(names);
+    const formattedNames = mediaArray.map((item, index) => ({
+        Id: item.Id,
+        formattedName: formattedNamesArray[index]
+    }));
+    formattedNamesToIdMap = mediaArray.reduce((map, item, index) => {
+            map[formattedNamesArray[index]] = item.Id;
+            return map;
+        },
+        {});
+    const segments = formattedNames.map(nameObj => ({
+        'fillStyle': "#c0c0c0",
+        'text': nameObj.formattedName
+    }));
+
+    canvas = document.getElementById("rouletteCanvas");
+    clickedWhatId = document.getElementById("clickedWhatId");
     canvasWidth = canvas.width;
     canvasHeight = canvas.height;
 
     theWheel = new Winwheel({
-        'canvasId': 'rouletteCanvas',
+        'canvasId': "rouletteCanvas",
         'outerRadius': canvasHeight / 2 - 20,
         'numSegments': 5,
         'textFontSize': 10,
         'innerRadius': 20,
         'responsive': true,
-        'textAlignment': 'center',
-        'textFontFamily': 'Georgia',
+        'textAlignment': "center",
+        'textFontFamily': "Georgia",
         'rotationAngle': -30,
-        'fillStyle': '#e7706f',
+        'fillStyle': "#e7706f",
         'lineWidth': 3,
-        'segments':
-            [
-                { 'fillStyle': '#c0c0c0', 'text': formattedNames[0] },
-                { 'fillStyle': '#c0c0c0', 'text': formattedNames[1] },
-                { 'fillStyle': '#c0c0c0', 'text': formattedNames[2] },
-                { 'fillStyle': '#c0c0c0', 'text': formattedNames[3] },
-                { 'fillStyle': '#c0c0c0', 'text': formattedNames[4] }
-            ],
+        'segments': segments,
         'animation':
         {
-            'type': 'spinToStop',
+            'type': "spinToStop",
             'duration': 3,
             'spins': 5,
-            'callbackFinished': 'winAnimation()',
-            'callbackAfter': 'drawPointer()'
+            'callbackFinished': "winAnimation(getIdByText)",
+            'callbackAfter': "drawPointer()"
         }
-        
+
     });
 
     drawPointer();
 
-    canvas.onclick = function (e) {
+    canvas.onclick = function(e) {
         resetSegmentColours();
-        let clickedSegment = theWheel.getSegmentAt(e.clientX, e.clientY);
+        const clickedSegment = theWheel.getSegmentAt(e.clientX, e.clientY);
         if (clickedSegment) {
-            clickedSegment.fillStyle = 'yellow';
+            clickedSegment.fillStyle = "yellow";
             theWheel.draw();
             drawPointer();
-            clickedWhat.innerText = clickedSegment.text;
+            clickedWhatId.innerText = getIdByText(clickedSegment.text);
         }
-    }
+    };
 
     function resetSegmentColours() {
         for (let x = 1; x < theWheel.segments.length; x++) {
-            theWheel.segments[x].fillStyle = '#c0c0c0';
+            theWheel.segments[x].fillStyle = "#c0c0c0";
         }
         theWheel.draw();
         drawPointer();
-        clickedWhat.innerText = "";
+        clickedWhatId.innerText = "";
     }
 
     function splitAndFormatStrings(strings) {
         return strings.map(str => {
-            let words = str.split(' ');
+            const words = str.split(" ");
+
             function wrapLongWord(word) {
                 const maxLength = 13;
-                let wrapped = [];
+                const wrapped = [];
                 while (word.length > maxLength) {
-                    wrapped.push(word.slice(0, maxLength) + '-');
+                    wrapped.push(word.slice(0, maxLength) + "-");
                     word = word.slice(maxLength);
                 }
                 wrapped.push(word);
-                return wrapped.join('\n');
+                return wrapped.join("\n");
             }
 
             let lines = [];
-            let currentLine = '';
+            let currentLine = "";
 
             words.forEach(word => {
-                let wrappedWord = word.length > 13 ? wrapLongWord(word) : word;
-                if ((currentLine + (currentLine ? ' ' : '') + wrappedWord).length <= 16) {
-                    currentLine += (currentLine ? ' ' : '') + wrappedWord;
+                const wrappedWord = word.length > 13 ? wrapLongWord(word) : word;
+                if ((currentLine + (currentLine ? " " : "") + wrappedWord).length <= 16) {
+                    currentLine += (currentLine ? " " : "") + wrappedWord;
                 } else {
                     lines.push(currentLine);
                     currentLine = wrappedWord;
@@ -96,28 +103,27 @@
 
             if (lines.length > 4) {
                 lines = lines.slice(0, 4);
-                lines[3] += '...'; 
+                lines[3] += "...";
             }
-            return lines.join('\n');
+            return lines.join("\n");
         });
     }
-    
-}
-function winAnimation() {
-    
-    let winningSegmentNumber = theWheel.getIndicatedSegmentNumber();
-    for (let x = 1; x < theWheel.segments.length; x++) {
-        theWheel.segments[x].fillStyle = '#c0c0c0';
-    }
-    theWheel.segments[winningSegmentNumber].fillStyle = 'yellow';
-    theWheel.draw();
-    drawPointer();
+
 }
 
-function alertPrize() {
-    let winningSegment = theWheel.getIndicatedSegment();
-    alert("You have won " + winningSegment.text + "!");
+function getIdByText(text) {
+    return formattedNamesToIdMap[text] || null;
 }
+
+function winAnimation(getIdByText) {
+    const winningSegmentNumber = theWheel.getIndicatedSegmentNumber();
+    theWheel.segments[winningSegmentNumber].fillStyle = "yellow";
+    theWheel.draw();
+    drawPointer();
+    const winningSegment = theWheel.getIndicatedSegment();
+    clickedWhatId.innerText = getIdByText(winningSegment.text);
+}
+
 
 function drawPointer() {
     ctx = theWheel.ctx;
@@ -125,8 +131,8 @@ function drawPointer() {
     canvasHeight = canvas.height;
 
     ctx.lineWidth = 2;
-    ctx.strokeStyle = 'black';
-    ctx.fillStyle = 'black';
+    ctx.strokeStyle = "black";
+    ctx.fillStyle = "black";
 
     const pointerWidth = 20;
     const pointerHeight = 20;
