8980dd9f32d1b738267edaf3c209f9730fe3bb22
Alexander Vostrikov <lanctole@yandex.ru>
2024-12-04 10:38:07 +0300

    Добавлена заглушка для отсутствующих изображений

--- ShikimoriSharp/Bases/SmallRepresentation.cs
+++ ShikimoriSharp/Bases/SmallRepresentation.cs
@@ -7,6 +7,6 @@ public class SmallRepresentation
     [JsonProperty("id")] public long Id { get; set; }
     [JsonProperty("name")] public string Name { get; set; }
     [JsonProperty("russian")] public string Russian { get; set; }
-    [JsonProperty("poster")] public Image Image { get; set; }
+    [JsonProperty("poster")] public Image? Image { get; set; }
     [JsonProperty("url")] public string Url { get; set; }
 }
